75d30df123bba621e0b3c827b8c17f13ad5f1368
zcf <78002854@qq.com>
2020-05-22 09:08:28 +0800

    提交java项目

---
+++ testgit/src/com/it/demo/MainClass.java
@@ -0,0 +1,10 @@
+package com.it.demo;
+
+/**
+ * Created by zcf on 2020/5/22.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
